882672b311991f82f681c24cc976948ec1167f6e
feri <rht.farideh@yahoo.com>
2023-12-11 11:32:23 +0100

    read data with API and configur the HTTP request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,26 @@
+import {useState, useEffect} from "react";
 function App() {
-
+const [products , setProducts]  = useState([{name : "product"} , {name: "product2"}]);
+useEffect(()=>{
+  fetch("http://localhost:5000/api/Products")
+  .then( response =>response.json())
+  .then( data => setProducts(data))
+},[])
+// function addProducts(){
+//   setProducts(prevState=> [...prevState, {name:"product" + (prevState.length+1)}])
+// }
 
   return (
       <div>
-       <h2>feri</h2>
+       <ul>
+       
+          {products.map((item , index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        
+       </ul>
+
+       {/* <button onClick={addProducts}> Add products</button> */}
       </div>
   )
 }
