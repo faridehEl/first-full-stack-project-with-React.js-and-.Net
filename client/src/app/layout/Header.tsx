5e25ef12c448d5bdfd8cef25d129a317e34d5520
feri <rht.farideh@yahoo.com>
2024-01-16 11:10:06 +0100

    adding NavLinks to Navbar

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,5 @@
-import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingBag, ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 const midleLink = [
     {title:'catalog' , path:'/catalog'},
@@ -17,12 +18,32 @@ export default function Header({darkMode , handelChange} : Props){
     return(
         <AppBar position="static" sx={{marginBottom:4}}>
             <Toolbar>
-            <Typography variant="h6">
+            <Typography variant="h6" component={NavLink} to={'/'} sx={{textDecoration:'none', color:'inherit' }}>
                 Re-Store
-                <Switch checked= {darkMode} onChange={handelChange}/>
             </Typography>
+            <Switch checked= {darkMode} onChange={handelChange}/>
             <List sx={{display:'flex'}}>
                 {midleLink.map(({title,path}) => (
+                    <ListItem
+                    component={NavLink}
+                    to ={path}
+                    key={path}
+                    sx={{color : 'inherit' , typography : 'h6'}}
+                    >
+                        {title.toUpperCase()}
+                    </ListItem>
+                ))}
+               
+            </List>
+            <IconButton color="inherit" size="large" edge="end" sx={{margin:1}}>
+                <Badge badgeContent='2' color="secondary">
+                    <ShoppingCart></ShoppingCart>
+                </Badge>
+
+            </IconButton>
+
+            <List sx={{display:'flex'}}>
+                {rightLink.map(({title,path}) => (
                     <ListItem
                     component={NavLink}
                     to ={path}
